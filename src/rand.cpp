37d5990bfc769aa384bc7b8724121fae39d0fe6d
Lucas Wiener <lucas@wiener.se>
2014-01-21 14:46:16 +0100

    Fixed rand bug

--- src/rand.cpp
+++ src/rand.cpp
@@ -10,19 +10,19 @@ void game::seed() {
 }
 
 unsigned int game::rand(unsigned int max, bool inclusive) {
-    if(!inclusive) {
-        if(max == 0) {
-            throw std::invalid_argument("Cannot have max 0 and exclusive.");
-        }
-        
-        max--;
+    if(max == 0 && inclusive) {
+        throw std::invalid_argument("Cannot have max 0 and inclusive.");
     }
     
     if(max >= RAND_MAX) {
         throw std::invalid_argument("max cannot be higher than MAX_RAND.");
     }
     
-    return (unsigned int)((std::rand() % (max == 0 ? 1 : max)) + (inclusive ? 1 : 0)); //Generates number between 1 and max inclusively.
+    if(max == 0) {
+        max++;
+    }
+    
+    return (unsigned int)((std::rand() % max) + (inclusive ? 1 : 0)); //Generates number between 1 and max inclusively.
 }
 
 bool game::happen(unsigned int limit, unsigned int max) {
